efd58e6c54562415d047850a3ca5356c7829099b
Cláudio Santos <claudiosantos@sistnet.com.br>
2022-01-02 19:47:22 -0300

    chore: router

---
+++ 05_express/07_routes/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+const basePath = path.join(__dirname, "templates");
+
+const user = require("./users");
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+app.use("/user", user);
+
+app.get("/", (req, res) => {
+  res.sendFile(`${basePath}/index.html`);
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
