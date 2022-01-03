e0d3b7b56d835b61fd417d4d9bf1675c3f23f674
Cláudio Santos <claudiosantos@sistnet.com.br>
2022-01-03 00:01:35 -0300

    chore: with

---
+++ 06_template_engine/07_with/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+app.get("/dashboard", (req, res) => {
+  const items = ["item a", "item b", "item c"];
+
+  res.render("dashboard", { items });
+});
+
+app.get("/post", (req, res) => {
+  const post = {
+    title: "Aprendendo Node.js",
+    category: "JavaScript",
+    body: "Este artigo vai te orientar a aprender NodeJS",
+    comments: 4,
+  };
+
+  res.render("blogpost", { post });
+});
+
+app.get("/", (req, res) => {
+  const user = {
+    name: "Claudio",
+    surname: "Santos",
+    age: 47,
+  };
+
+  const palavra = "Teste";
+
+  const auth = true;
+
+  const approved = false;
+
+  res.render("home", { user: user, palavra, auth, approved });
+});
+
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
